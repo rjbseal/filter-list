1306feba42d4d0c8dc92721f8e6fc84c3bbeb03b
Robin <rjblackborow@gmail.com>
2022-10-07 20:59:14 +0100

    added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Array to populate <li>
 const listArray = [
     "Steve",
     "Max",
@@ -12,8 +13,11 @@ const listArray = [
     "Nancy"
 ]
 
+// Get DOM elements
 const filterInput = document.querySelector(".filter-input");
 const output = document.querySelector(".output");
+
+// load list elements to page dynamically
 window.addEventListener("DOMContentLoaded", loadList(listArray))
 
 function loadList(arr) {
@@ -30,12 +34,19 @@ function loadList(arr) {
 }
 
 filterInput.addEventListener("input", (e) => {
+    // store user input and convert to lower case
     let userInput = e.target.value.toLowerCase();
+
+    // if listArray contains same element as the users input
+    // then return new array with matching element
     const result = listArray.filter(item => item.toLowerCase().includes(userInput))
 
+    // display the matching element(s) to the DOM 
     loadList(result)
 })
 
+// fade in each <li> with 0.1 sec delay for each one
+// fade in effect should take 0.2 sec
 function fadeInList () {
     $('.list-item').each( function (i) {
         $(this).css('opacity', 0);
